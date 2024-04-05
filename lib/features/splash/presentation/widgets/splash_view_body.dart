ddd3b742771f230272a150619e3855dbfc154fc1
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-05 10:47:09 +0200

    added animation in splash view

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,20 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+
+    animationController?.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         const Spacer(flex: 2),
-        const Text(
-          'Fruit Market',
-          style: TextStyle(
-            fontSize: 51,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Poppins',
-            color: Color(0xFFFFFFFF),
+        FadeTransition(
+          opacity: fadingAnimation!,
+          child: const Text(
+            'Fruit Market',
+            style: TextStyle(
+              fontSize: 51,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Poppins',
+              color: Color(0xFFFFFFFF),
+            ),
           ),
         ),
         const Spacer(),
