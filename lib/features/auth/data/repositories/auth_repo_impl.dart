52c0cb38012db9eb1bc37f2c03e64391f391e3da
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-06 15:59:56 +0200

    implement facebook and google login, handling exceptions, fix build gradle

---
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../../domain/repositories/auth_repo.dart';
+
+class AuthRepoImp extends AuthRepo {
+  @override
+  Future completeInformation({String? name, String? phone, String? address}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future loginWithFacebook() async {
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
+  @override
+  Future loginWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
